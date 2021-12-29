ad486a7c42b92e02c0da6b7d09b928fda5e98d9d
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-12-29 13:02:20 +0200

    set the state and try to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Form } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [name, setName] = useState('');
+  const [userName, setUsername] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/example')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className='App'>
+      <div className='navbar'>
+        <h1>Github Search</h1>{' '}
+      </div>
+      <div className='search'>
+        <form className='SearchForm'>
+          <button type='submit' className='SearchForm-button'>
+            <span className='SearchForm-button-label'>Search</span>
+          </button>
+
+          <input
+            className='SearchForm-input'
+            //value={query}
+            type='text'
+            autoComplete='off'
+            autoFocus
+            placeholder='Search Github User'
+            // checked={completed}
+            //onChange={handleChange}
+          />
+        </form>
+      </div>
+
+      {/* Карточка */}
+
+      <div className='card'>
+        <img src='../w3images/team2.jpg' className='userImg' alt='John' />
+        <h2>Name</h2>
+        <p className='title'>Title</p>
+        <div className='innerDiv'>
+          <a href='#'>
+            <i className='fa fa-dribbble'></i>
+          </a>
+          <a href='#'>
+            <i className='fa fa-twitter'></i>
+          </a>
+          <a href='#'>
+            <i className='fa fa-linkedin'></i>
+          </a>
+          <a href='#'>
+            <i className='fa fa-facebook'></i>
+          </a>
+        </div>
+        <p>
+          <button>Contacts</button>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
